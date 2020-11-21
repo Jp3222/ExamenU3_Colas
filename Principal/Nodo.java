2b8161d4b450a4039e3de1cabf847350258beddd
Juan Pablo Campos <64780248+Jp3222@users.noreply.github.com>
2020-11-20 22:59:19 -0600

    Create Nodo.java

---
+++ Principal/Nodo.java
@@ -0,0 +1,17 @@
+package examenu3_colas2;
+
+public class Nodo {
+
+    String tipo;
+    boolean vip;
+    int i;
+    Nodo siguiente;
+
+    public Nodo(String t, boolean v, int i) {
+        this.tipo = t;
+        this.vip = v;
+        this.i = i;
+        this.siguiente = null;
+    }
+
+}
